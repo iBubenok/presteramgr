680dc96fc7a03279ba5c602e2582e7e5a03e38a3
Oleg Belozeorov <upwawet@gmail.com>
2011-03-14 19:03:06 +0500

    начал реализацию обработки событий в базовом режиме

--- src/cpss-event.c
+++ src/cpss-event.c
@@ -19,53 +19,47 @@ static CPSS_UNI_EV_CAUSE_ENT events [] = {
 static GT_UINTPTR event_handle;
 
 
-static GT_VOID
-event_isr_cb (GT_UINTPTR hndl, GT_VOID *cookie)
-{
-  GT_STATUS rc;
-  static GT_U32 ebmp [CPSS_UNI_EV_BITMAP_SIZE_CNS];
-  GT_U32 edata;
-  GT_U8 dev;
-
-  rc = CRP (cpssEventWaitingEventsGet (event_handle, ebmp,
-                                       CPSS_UNI_EV_BITMAP_SIZE_CNS));
-  if (rc != GT_OK)
-    return;
-  osPrintSync ("waiting: %08X\n",
-               ebmp [CPSS_PP_PORT_LINK_STATUS_CHANGED_E >> 5]);
-
-  while ((rc = CRP (cpssEventRecv (event_handle,
-                                   CPSS_PP_PORT_LINK_STATUS_CHANGED_E,
-                                   &edata, &dev))) == GT_OK)
-    osPrintSync ("link change on dev %d port %2d\n",
-                 dev, edata);
-
-  osPrintSync ("clearing treated events\n");
-  CRP (cpssEventTreatedEventsClear (event_handle));
-  osPrintSync ("done\n");
-}
-
 GT_STATUS
 cpss_bind_events (void)
 {
   GT_STATUS rc;
   int i;
+  static GT_U32 ebmp [CPSS_UNI_EV_BITMAP_SIZE_CNS];
+  GT_U32 edata;
+  GT_U8 dev;
 
   extDrvUartInit ();
 
-  rc = CRP (cpssEventIsrBind (events, EVENT_NUM, event_isr_cb, NULL, &event_handle));
+  rc = CRP (cpssEventBind (events, EVENT_NUM, &event_handle));
   if (rc != GT_OK)
     return rc;
 
   for (i = 0; i < EVENT_NUM; ++i) {
-    rc = CRP (cpssEventDeviceMaskSet (0, events [i], CPSS_EVENT_UNMASK_E));
+    rc = CRP (cpssEventDeviceMaskSet (0, events [i],
+                                      CPSS_EVENT_UNMASK_E));
     if (rc != GT_OK)
       return rc;
   }
 
-  rc = CRP (cpssEventTreatedEventsClear (event_handle));
-  if (rc != GT_OK)
-    return rc;
+  while (1) {
+    rc = CRP (cpssEventSelect (event_handle, NULL, ebmp,
+                               CPSS_UNI_EV_BITMAP_SIZE_CNS));
+    if (rc != GT_OK)
+      return rc;
+
+    while ((rc = cpssEventRecv (event_handle,
+                                CPSS_PP_PORT_LINK_STATUS_CHANGED_E,
+                                &edata, &dev)) == GT_OK) {
+      GT_BOOL link;
+
+      rc = CRP (cpssDxChPortLinkStatusGet (dev, (GT_U8) edata, &link));
+      if (rc != GT_OK)
+        return rc;
+
+      osPrintSync ("dev %d port %2d link %s\n",
+                   dev, edata, link ? "up" : "down");
+    }
+  }
 
   return GT_OK;
 }
