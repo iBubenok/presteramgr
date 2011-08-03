18d16756151cd97ba55b3fbd4be1c4c10d794094
Oleg Belozeorov <upwawet@gmail.com>
2011-08-03 15:35:20 +0600

    basic netlink mgmt communication works

--- src/mgmt.c
+++ src/mgmt.c
@@ -8,52 +8,108 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
+#include <pthread.h>
 
 #define NETLINK_PDSA_MGMT 30
 
 #define MAX_PAYLOAD 1024
 
 static int sock;
-static struct sockaddr_nl src_addr, dst_addr;
-static unsigned char _buf [NLMSG_SPACE (MAX_PAYLOAD)];
-static struct nlmsghdr *nlh = (struct nlmsghdr *) _buf;
-static struct iovec iov;
-static struct msghdr msg;
+static pid_t my_pid;
+static pthread_t thread;
+
+static int
+mgmt_tx (pid_t to, __u16 type, const void *data, size_t len)
+{
+  struct iovec iov[2];
+  static struct msghdr msg;
+  struct nlmsghdr nlh;
+  struct sockaddr_nl addr;
+
+  nlh.nlmsg_len = NLMSG_SPACE (len);
+  nlh.nlmsg_type = type;
+  nlh.nlmsg_flags = 0;
+  nlh.nlmsg_seq = 0;
+  nlh.nlmsg_pid = my_pid;
+
+  iov[0].iov_base = &nlh;
+  iov[0].iov_len = NLMSG_HDRLEN;
+  iov[1].iov_base = (void *) data;
+  iov[1].iov_len = len;
+
+  addr.nl_family = AF_NETLINK;
+  addr.nl_pad = 0;
+  addr.nl_pid = to;
+  addr.nl_groups = 0;
+
+  msg.msg_name = (void *) &addr;
+  msg.msg_namelen = sizeof (addr);
+  msg.msg_iov = iov;
+  msg.msg_iovlen = 2;
+
+  return sendmsg (sock, &msg, 0);
+}
+
+static void *
+mgmt_thread (void *unused)
+{
+  struct sockaddr_nl addr;
+  static unsigned char buf [NLMSG_SPACE (MAX_PAYLOAD)];
+  struct nlmsghdr *nlh = (struct nlmsghdr *) buf;
+  struct msghdr msg;
+  struct iovec iov;
+
+  iov.iov_base = buf;
+  iov.iov_len = NLMSG_SPACE (MAX_PAYLOAD);
+
+  msg.msg_name = (void *) &addr;
+  msg.msg_namelen = sizeof (addr);
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+
+  printf ("receiving messages from kernel\n");
+  while (1) {
+    if (recvmsg (sock, &msg, 0) < 0) {
+      perror ("recvmsg");
+      break;
+    }
+
+    /* TODO: handle messages. */
+    printf ("Received message: %s\n", (char *) NLMSG_DATA (nlh));
+  }
+
+  return NULL;
+}
 
 int
 mgmt_init (void)
 {
+  static struct sockaddr_nl addr;
+  const char *str = "XPEH BAM BCEM B POT!";
+
+  my_pid = getpid ();
+
   sock = socket (PF_NETLINK, SOCK_RAW, NETLINK_PDSA_MGMT);
   if (sock < 0) {
     perror ("socket");
     return -1;
   }
 
-  memset (&src_addr, 0, sizeof (src_addr));
-  src_addr.nl_family = AF_NETLINK;
-  src_addr.nl_pid = getpid ();
-  bind (sock, (struct sockaddr*) &src_addr, sizeof (src_addr));
-
-  memset (&dst_addr, 0, sizeof (dst_addr));
-  dst_addr.nl_family = AF_NETLINK;
-
-  memset (nlh, 0, NLMSG_SPACE (MAX_PAYLOAD));
-  nlh->nlmsg_len = NLMSG_SPACE (MAX_PAYLOAD);
-  nlh->nlmsg_pid = getpid ();
-  nlh->nlmsg_flags = 0;
-  strcpy (NLMSG_DATA (nlh), "XPEH BAM B POT!");
-  iov.iov_base = (void *) nlh;
-  iov.iov_len = nlh->nlmsg_len;
-  msg.msg_name = (void *) &dst_addr;
-  msg.msg_namelen = sizeof (dst_addr);
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
+  memset (&addr, 0, sizeof (addr));
+  addr.nl_family = AF_NETLINK;
+  addr.nl_pid = my_pid;
+  if (bind (sock, (struct sockaddr*) &addr, sizeof (addr)) < 0) {
+    perror ("bind");
+    return -1;
+  }
 
   printf ("Sending message to kernel\n");
-  sendmsg (sock, &msg, 0);
+  if (mgmt_tx (0, 115, str, strlen (str) + 1) < 0) {
+    perror ("mgmt_tx");
+    return -1;
+  }
 
-  recvmsg (sock, &msg, 0);
-  printf ("Received message payload: %s\n", (char *) NLMSG_DATA (nlh));
+  pthread_create (&thread, NULL, mgmt_thread, NULL);
 
   return 0;
 }
