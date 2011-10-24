99e83485dbf106a549bc64446e4820d3031f417f
Oleg Belozeorov <upwawet@gmail.com>
2011-10-24 15:49:05 +0600

    store port state
    closes #22

--- src/port.h
+++ src/port.h
@@ -1,6 +1,15 @@
+#ifndef __PORT_H__
+#define __PORT_H__
+
+
+struct port_state {
+  CPSS_PORT_ATTRIBUTES_STC attrs;
+};
+
 struct port {
   GT_U8 ldev;
   GT_U8 lport;
+  struct port_state state;
 };
 
 extern struct port *ports;
@@ -15,3 +24,5 @@ port_valid (int n)
 extern int port_init (void);
 extern GT_STATUS port_set_sgmii_mode (int);
 extern int port_exists (GT_U8, GT_U8);
+
+#endif /* __PORT_H__ */
