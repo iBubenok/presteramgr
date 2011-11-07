13f3223a35f2d18135530df911d65e0763d82842
Oleg Belozeorov <upwawet@gmail.com>
2011-11-07 14:23:48 +0600

    test CC_PORT_GET_STATE

--- src/pmntest.c
+++ src/pmntest.c
@@ -6,8 +6,6 @@
 #include <czmq.h>
 #include <control-proto.h>
 
-static command_t data = 0;
-
 static int
 notify_handler (zloop_t *loop, zmq_pollitem_t *pi, void *sock)
 {
@@ -32,46 +30,12 @@ notify_handler (zloop_t *loop, zmq_pollitem_t *pi, void *sock)
   memcpy (&state, zframe_data (frame), sizeof (state));
   zframe_destroy (&frame);
   if (state.link)
-    printf ("port %d link up at speed %d, %s duplex\n",
+    printf ("port %2d link up at speed %d, %s duplex\n",
             port, state.speed, state.duplex ? "full" : "half");
   else
-    printf ("port %d link down\n", port);
+    printf ("port %2d link down\n", port);
 
   zmsg_destroy (&msg);
-  return 0;
-}
-
-static int
-req_timer (zloop_t *loop, zmq_pollitem_t *dummy, void *sock)
-{
-  zmsg_t *msg = zmsg_new ();
-
-  data++;
-  zmsg_addmem (msg, &data, sizeof (data));
-  zmsg_send (&msg, sock);
-
-  fprintf (stderr, "sent request: %d\n", data);
-
-  return 0;
-}
-
-static int
-reply_handler (zloop_t *loop, zmq_pollitem_t *pi, void *sock)
-{
-  zmsg_t *msg = zmsg_recv (sock);
-  zframe_t *frame;
-  status_t data;
-
-  frame = zmsg_pop (msg);
-  assert (zframe_size (frame) == sizeof (data));
-  memcpy (&data, zframe_data (frame), sizeof (data));
-  zframe_destroy (&frame);
-  zmsg_destroy (&msg);
-
-  fprintf (stderr, "got reply: %d\n", data);
-
-  zloop_timer (loop, 1000, 1, req_timer, sock);
-
   return 0;
 }
 
@@ -82,8 +46,8 @@ main (int argc, char **argv)
   void *sub_sock = zsocket_new (context, ZMQ_SUB);
   zmq_pollitem_t pi = { sub_sock, 0, ZMQ_POLLIN };
   void *cmd_sock = zsocket_new (context, ZMQ_REQ);
-  zmq_pollitem_t pi2 = { cmd_sock, 0, ZMQ_POLLIN };
   zloop_t *loop = zloop_new ();
+  port_num_t port;
 
   zmq_setsockopt (sub_sock, ZMQ_UNSUBSCRIBE, "", 0);
   control_notification_subscribe (sub_sock, CN_PORT_LINK_STATE);
@@ -91,10 +55,42 @@ main (int argc, char **argv)
 
   zsocket_connect (cmd_sock, CMD_SOCK_EP);
 
-  zloop_poller (loop, &pi, notify_handler, sub_sock);
-  zloop_poller (loop, &pi2, reply_handler, cmd_sock);
-  zloop_timer (loop, 1000, 1, req_timer, cmd_sock);
+  for (port = 0; port < 30; port++) {
+    zmsg_t *msg = zmsg_new ();
+    command_t command = CC_PORT_GET_STATE;
+    zmsg_addmem (msg, &command, sizeof (command));
+    zmsg_addmem (msg, &port, sizeof (port));
+    zmsg_send (&msg, cmd_sock);
 
+    msg = zmsg_recv (cmd_sock);
+    assert (zmsg_size (msg) > 0);
+    zframe_t *frame = zmsg_pop (msg);
+    status_t status;
+    assert (zframe_size (frame) == sizeof (status));
+    memcpy (&status, zframe_data (frame), sizeof (status));
+    zframe_destroy (&frame);
+    if (status != ST_OK) {
+      printf ("port %2d: CC_PORT_GET_STATE returned %d\n", port, status);
+      zmsg_destroy (&msg);
+      continue;
+    }
+
+    struct port_link_state state;
+    frame = zmsg_pop (msg);
+    assert (zframe_size (frame) == sizeof (state));
+    memcpy (&state, zframe_data (frame), sizeof (state));
+    zframe_destroy (&frame);
+
+    if (state.link)
+      printf ("port %2d link up at speed %d, %s duplex\n",
+              port, state.speed, state.duplex ? "full" : "half");
+    else
+      printf ("port %2d link down\n", port);
+
+    zmsg_destroy (&msg);
+  }
+
+  zloop_poller (loop, &pi, notify_handler, sub_sock);
   zloop_start (loop);
 
   return 0;
