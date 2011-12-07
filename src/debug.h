ad6fd8fc1b0c33965cd3afe65d9f2ac281c0cc22
Oleg Belozeorov <upwawet@gmail.com>
2011-12-07 14:33:23 +0600

    define SHOW() for GT_STATUS; use it in CRP macros

--- src/debug.h
+++ src/debug.h
@@ -1,34 +1,38 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#define DECLSHOW(type) extern const char *show_##type (type)
+#define SHOW(type, value) (show_##type (value))
+
+DECLSHOW (GT_STATUS);
+
 #if (CPSS_RPRINT == 1)
 #include <stdio.h>
-#define CRP(call) ({                            \
-  GT_STATUS __st = (call);                      \
-  if (__st != GT_OK)                            \
-    osPrintSync ("%s:%d: %s => %04X\n",         \
-             __FILE__, __LINE__, #call, __st);  \
-  __st;                                         \
+#define CRP(call) ({                                                    \
+      GT_STATUS __st = (call);                                          \
+      if (__st != GT_OK)                                                \
+        osPrintSync ("%s:%d: %s => %s (%04X)\n",                        \
+                     __FILE__, __LINE__, #call,                         \
+                     SHOW (GT_STATUS, __st), __st);                     \
+      __st;                                                             \
     })
 #elif (CPSS_RPRINT == 2)
 #include <stdio.h>
-#define CRP(call) ({                            \
-  GT_STATUS __st = (call);                      \
-  osPrintSync ("%s:%d: %s => %04X\n",           \
-           __FILE__, __LINE__, #call, __st);    \
-  __st;                                         \
+#define CRP(call) ({                                 \
+      GT_STATUS __st = (call);                       \
+      osPrintSync ("%s:%d: %s => %s (%04X)\n",       \
+                   __FILE__, __LINE__, #call,        \
+                   SHOW (GT_STATUS, __st), __st);    \
+      __st;                                          \
     })
 #else
 #define CRP(call) call
 #endif /* CPSS_RPRINT */
 
 #define CRPR(call) ({                           \
-  GT_STATUS __st = CRP (call);                  \
-  if (__st != GT_OK)                            \
-    return __st;                                \
+      GT_STATUS __st = CRP (call);              \
+      if (__st != GT_OK)                        \
+        return __st;                            \
     })
-
-#define DECLSHOW(type) extern const char *show_##type (type)
-#define SHOW(type, value) (show_##type (value))
 
 #endif /* __DEBUG_H__ */
