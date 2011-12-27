042a9ffc91bb5836d162d37789266473d6bfd61d
Oleg Belozeorov <upwawet@gmail.com>
2011-12-27 12:24:51 +0600

    implement CC_ADD_STATIC_MAC

--- src/mac.c
+++ src/mac.c
@@ -2,8 +2,13 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#include <cpss/dxCh/dxChxGen/bridge/cpssDxChBrgFdb.h>
+
 #include <mac.h>
 #include <port.h>
+#include <vlan.h>
+#include <debug.h>
+
 
 /*
  * TODO: maintain shadow FDB.
@@ -12,21 +17,31 @@
 enum status
 mac_add_static (const struct mac_op_arg *arg)
 {
-  if (arg->vid < 1)
+  CPSS_MAC_ENTRY_EXT_STC me;
+  GT_STATUS result;
+
+  if (!vlan_valid (arg->vid))
     return ST_BAD_VALUE;
 
   if (!port_valid (arg->port))
     return ST_BAD_VALUE;
 
-  /* TODO: really do it */
-  fprintf (stderr,
-           "ADD STATIC MAC (AAAAAAA!):\r\n"
-           "\tport = %d\r\n"
-           "\tvlan = %d\r\n"
-           "\taddr = %02X:%02X:%02X:%02X:%02X:%02X\r\n",
-           arg->port, arg->vid,
-           arg->mac[0], arg->mac[1], arg->mac[2],
-           arg->mac[3], arg->mac[4], arg->mac[5]);
+  memset (&me, 0, sizeof (me));
+  me.key.entryType = CPSS_MAC_ENTRY_EXT_TYPE_MAC_ADDR_E;
+  memcpy (me.key.key.macVlan.macAddr.arEther, arg->mac, sizeof (arg->mac));
+  me.key.key.macVlan.vlanId = arg->vid;
+  me.dstInterface.type = CPSS_INTERFACE_PORT_E;
+  me.dstInterface.devPort.devNum = ports[arg->port].ldev;
+  me.dstInterface.devPort.portNum = ports[arg->port].lport;
+  me.isStatic = GT_TRUE;
+  me.daCommand = CPSS_MAC_TABLE_FRWRD_E;
+  me.saCommand = CPSS_MAC_TABLE_FRWRD_E;
 
-  return ST_OK;
+  result = CRP (cpssDxChBrgFdbMacEntrySet (0, &me));
+  switch (result) {
+  case GT_OK:        return ST_OK;
+  case GT_HW_ERROR:  return ST_HW_ERROR;
+  case GT_BAD_STATE: return ST_BUSY;
+  default:           return ST_HEX;
+  }
 }
