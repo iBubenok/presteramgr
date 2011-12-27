9fd3755cb4a8c20895c6f36757131177dd19b063
Oleg Belozeorov <upwawet@gmail.com>
2011-12-27 12:23:26 +0600

    add VID validity check

--- src/vlan.h
+++ src/vlan.h
@@ -3,6 +3,13 @@
 
 #include <control-proto.h>
 
+
+static inline int
+vlan_valid (vid_t vid)
+{
+  return vid > 0 && vid < 4096;
+}
+
 extern int vlan_init (void);
 extern enum status vlan_dump (vid_t);
 extern GT_STATUS vlan_set_mac_addr (GT_U16, const unsigned char *);
