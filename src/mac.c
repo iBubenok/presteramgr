50d7e95d8dba690b7a9b45afb644e1b4c8ac47aa
Oleg Belozeorov <upwawet@gmail.com>
2012-01-06 17:04:37 +0600

    test FDB upload

--- src/mac.c
+++ src/mac.c
@@ -115,8 +115,8 @@ mac_list (void)
   GT_U32 pt, pt_mask;
   GT_BOOL done = GT_FALSE;
   GT_BOOL full;
-  /* GT_U32 naddrs = 20000, i; */
-  /* static CPSS_MAC_UPDATE_MSG_EXT_STC addrs[20000]; */
+  GT_U32 naddrs = 20000, i;
+  static CPSS_MAC_UPDATE_MSG_EXT_STC addrs[20000];
 
   CRP (cpssDxChBrgFdbQueueFullGet (0, CPSS_DXCH_FDB_QUEUE_TYPE_AU_E, &full));
   fprintf (stderr, "AU queue full: %d\r\n", full);
@@ -140,20 +140,40 @@ mac_list (void)
   CRP (cpssDxChBrgFdbUploadEnableSet (0, GT_TRUE));
   CRP (cpssDxChBrgFdbTrigActionStart (0, CPSS_FDB_ACTION_AGE_WITHOUT_REMOVAL_E));
 
-  /* do { */
-  /*   CRP (cpssDxChBrgFdbTrigActionStatusGet (0, &done)); */
-  /* } while (!done); */
+  do {
+    CRP (cpssDxChBrgFdbTrigActionStatusGet (0, &done));
+  } while (!done);
 
-  /* CRP (cpssDxChBrgFdbUploadEnableSet (0, GT_FALSE)); */
-  /* CRP (cpssDxChBrgFdbActionActiveInterfaceSet (0, is_trunk, is_trunk_mask, pt, pt_mask)); */
-  /* CRP (cpssDxChBrgFdbActionActiveDevSet (0, act_dev, act_dev_mask)); */
-  /* CRP (cpssDxChBrgFdbActionModeSet (0, act_mode)); */
-  /* CRP (cpssDxChBrgFdbActionActiveVlanSet (0, act_vid, act_vid_mask)); */
-  /* CRP (cpssDxChBrgFdbMacTriggerModeSet (0, mac_mode)); */
-  /* CRP (cpssDxChBrgFdbActionsEnableSet (0, GT_TRUE)); */
+  CRP (cpssDxChBrgFdbUploadEnableSet (0, GT_FALSE));
+  CRP (cpssDxChBrgFdbActionActiveInterfaceSet (0, is_trunk, is_trunk_mask, pt, pt_mask));
+  CRP (cpssDxChBrgFdbActionActiveDevSet (0, act_dev, act_dev_mask));
+  CRP (cpssDxChBrgFdbActionModeSet (0, act_mode));
+  CRP (cpssDxChBrgFdbActionActiveVlanSet (0, act_vid, act_vid_mask));
+  CRP (cpssDxChBrgFdbMacTriggerModeSet (0, mac_mode));
+  CRP (cpssDxChBrgFdbActionsEnableSet (0, GT_TRUE));
 
-  /* CRP (cpssDxChBrgFdbFuMsgBlockGet (0, &naddrs, addrs)); */
-  /* fprintf (stderr, "*** GOT %lu MAC addrs\r\n", naddrs); */
+  CRP (cpssDxChBrgFdbFuMsgBlockGet (0, &naddrs, addrs));
+  fprintf (stderr, "*** GOT %lu MAC addrs\r\n", naddrs);
+
+  for (i = 0; i < naddrs; i++) {
+    port_num_t n;
+
+    if ((!addrs[i].skip) &&
+        (addrs[i].updType == CPSS_FU_E) &&
+        (addrs[i].macEntry.key.entryType == CPSS_MAC_ENTRY_EXT_TYPE_MAC_ADDR_E) &&
+        (addrs[i].macEntry.dstInterface.type == CPSS_INTERFACE_PORT_E) &&
+        (n = port_num (addrs[i].macEntry.dstInterface.devPort.devNum,
+                       addrs[i].macEntry.dstInterface.devPort.portNum))) {
+      fprintf (stderr, "VLAN %4d, port %2d: %02X:%02X:%02X:%02X:%02X:%02X\r\n",
+               addrs[i].macEntry.key.key.macVlan.vlanId, n,
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[0],
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[1],
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[2],
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[3],
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[4],
+               addrs[i].macEntry.key.key.macVlan.macAddr.arEther[5]);
+    }
+  }
 
   return ST_OK;
 }
