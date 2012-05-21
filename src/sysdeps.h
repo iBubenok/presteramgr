26bdbfce5951d180abca011888dbd9b947a65a7e
Oleg Belozeorov <upwawet@gmail.com>
2012-05-21 13:59:28 +0600

    define port-related constants for Arlan-3424FE and SM-12F

--- src/sysdeps.h
+++ src/sysdeps.h
@@ -1,9 +1,52 @@
 #ifndef __SYSDEPS_H__
 #define __SYSDEPS_H__
 
-/* TODO: make the constants device-dependent/configurable. */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#if defined (VARIANT_ARLAN_3424_FE)
 #define NDEVS 1
 #define NPORTS 28
+#define DECLARE_PORT_MAP(name)                  \
+  int name[NPORTS] = {                          \
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,   \
+    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, \
+    27, 26, 25, 24                              \
+  }
+
+static inline int IS_FE_PORT (int n)
+{
+  return n >= 0 && n < 24;
+}
+
+static inline int IS_GE_PORT (int n)
+{
+  return n >= 24 && n < 28;
+}
+
+#elif defined (VARIANT_SM_12F)
+#define NDEVS 1
+#define NPORTS 16
+#define DECLARE_PORT_MAP(name)                      \
+  int name[NPORTS] = {                              \
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, \
+    27, 26, 25, 24                                  \
+  }
+
+static inline int IS_FE_PORT (int n)
+{
+  return n >= 0 && n < 12;
+}
+
+static inline int IS_GE_PORT (int n)
+{
+  return n >= 12 && n < 16;
+}
+
+#else
+#error Undefined or unsupported variant.
+#endif /* VARIANT_* */
 
 extern size_t sysdeps_default_stack_size;
 
