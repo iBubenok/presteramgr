42559c75f3b35cc53b231273803b1057c9834b99
Oleg Belozeorov <upwawet@gmail.com>
2012-06-23 16:53:24 +0600

    rtnl: receive NEIGH notifications

--- src/rtnl.c
+++ src/rtnl.c
@@ -31,6 +31,69 @@ nl_mgrp (__u32 group)
   return group ? (1 << (group - 1)) : 0;
 }
 
+#ifndef NDA_RTA
+#define NDA_RTA(r) \
+	((struct rtattr*) (((char*) (r)) + NLMSG_ALIGN (sizeof (struct ndmsg))))
+#endif
+
+static int
+parse_rtattr (struct rtattr *tb[], int max, struct rtattr *rta, int len)
+{
+  memset (tb, 0, sizeof (struct rtattr *) * (max + 1));
+  while (RTA_OK (rta, len)) {
+    if ((rta->rta_type <= max) && (!tb[rta->rta_type]))
+      tb[rta->rta_type] = rta;
+    rta = RTA_NEXT (rta,len);
+  }
+  if (len)
+    ERR ("deficit %d, rta_len=%d\n", len, rta->rta_len);
+  return 0;
+}
+
+static void
+print_neigh (struct nlmsghdr *nlh)
+{
+  struct ndmsg *d = NLMSG_DATA (nlh);
+  struct rtattr *a[NDA_MAX+1];
+  unsigned char *p;
+
+  if (nlh->nlmsg_len < NLMSG_LENGTH (sizeof (*d))) {
+    ERR ("invalid nlmsg len %d", nlh->nlmsg_len);
+    return;
+  }
+
+  parse_rtattr (a, NDA_MAX, NDA_RTA (d),
+                nlh->nlmsg_len - NLMSG_LENGTH (sizeof (*d)));
+
+  if (!a[NDA_LLADDR] || !a[NDA_DST])
+    return;
+
+  p = RTA_DATA (a[NDA_DST]);
+  INFO ("dst: %d %d.%d.%d.%d",
+        RTA_PAYLOAD (a[NDA_DST]), p[0], p[1], p[2], p[3]);
+
+  p = RTA_DATA (a[NDA_LLADDR]);
+  INFO ("lladdr %d %02X:%02X:%02X:%02X:%02X:%02X",
+        RTA_PAYLOAD (a[NDA_LLADDR]), p[0], p[1], p[2], p[3], p[4], p[5]);
+}
+
+static void
+rtnl_handle_msg (struct nlmsghdr *nlh)
+{
+  switch (nlh->nlmsg_type) {
+  case RTM_NEWNEIGH:
+    DEBUG ("new neighbour");
+    print_neigh (nlh);
+    break;
+  case RTM_DELNEIGH:
+    DEBUG ("deleted neighbour");
+    print_neigh (nlh);
+    break;
+  default:
+    break;
+  }
+}
+
 static void *
 rtnl_handler (void *unused)
 {
@@ -39,9 +102,7 @@ rtnl_handler (void *unused)
   struct nlmsghdr *nlh = (struct nlmsghdr *) buf;
   struct msghdr msg;
   struct iovec iov;
-
-  iov.iov_base = buf;
-  iov.iov_len = NLMSG_SPACE (MAX_PAYLOAD);
+  int rc;
 
   msg.msg_name = (void *) &addr;
   msg.msg_namelen = sizeof (addr);
@@ -49,13 +110,43 @@ rtnl_handler (void *unused)
   msg.msg_iovlen = 1;
 
   DEBUG ("receiving messages from kernel");
+  iov.iov_base = buf;
   while (1) {
-    if (recvmsg (fd, &msg, 0) < 0) {
+    iov.iov_len = NLMSG_SPACE (MAX_PAYLOAD);
+    if ((rc = recvmsg (fd, &msg, 0)) < 0) {
       ERR ("recvmsg(): %s", strerror (errno));
       break;
     }
-    INFO ("got message from kernel");
-    /* TODO: handle msg. */
+
+    INFO ("got message from kernel, len=%d", rc);
+    for (nlh = (struct nlmsghdr*) buf; rc >= sizeof (*nlh); ) {
+      int len = nlh->nlmsg_len;
+      int l = len - sizeof (*nlh);
+
+      if (l < 0 || len > rc) {
+        if (msg.msg_flags & MSG_TRUNC)
+          ERR ("truncated message");
+        else
+          ERR ("malformed message: len=%d", len);
+        exit (1);
+      }
+
+      INFO ("processing payload");
+      rtnl_handle_msg (nlh);
+
+      rc -= NLMSG_ALIGN (len);
+      nlh = (struct nlmsghdr*) ((char*) nlh + NLMSG_ALIGN (len));
+    }
+
+    if (msg.msg_flags & MSG_TRUNC) {
+      INFO ("message truncated\n");
+      continue;
+    }
+
+    if (rc) {
+      ERR ("remnant of size %d\n", rc);
+      exit (1);
+	}
   }
 
   return NULL;
