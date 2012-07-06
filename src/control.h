c8efa1e88d73275e62715d72602c79b47a897341
Oleg Belozeorov <upwawet@gmail.com>
2012-07-06 16:12:56 +0600

    control: add internal command codes

--- src/control.h
+++ src/control.h
@@ -11,4 +11,10 @@ extern int control_start (void);
 extern void control_notify_port_state (port_id_t, const CPSS_PORT_ATTRIBUTES_STC *);
 extern void control_notify_spec_frame (port_id_t, uint8_t, const unsigned char *, size_t);
 
+enum control_int_command {
+  CC_INT_ROUTE_ADD_PREFIX = CC_MAX,
+  CC_INT_ROUTE_DEL_PREFIX,
+  CC_INT_MAX
+};
+
 #endif /* __CONTROL_H__ */
