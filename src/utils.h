279874de2e0bdb5d69bb4128d1586df0edb0e7ed
Oleg Belozeorov <upwawet@gmail.com>
2012-08-29 17:18:00 +0600

    add generic bitmap access macros

--- src/utils.h
+++ src/utils.h
@@ -4,4 +4,8 @@
 #define ARRAY_SIZE(a) (sizeof (a) / sizeof (*a))
 #define ON_GT_ERROR(rc) if ((rc) != GT_OK)
 
+#define GET_BIT(bmp, n) (!!(bmp[n / sizeof (*bmp)] & (1 << (n % sizeof (*bmp)))))
+#define SET_BIT(bmp, n) (bmp[n / sizeof (*bmp)] |= (1 << (n % sizeof (*bmp))))
+#define CLR_BIT(bmp, n) (bmp[n / sizeof (*bmp)] &= ~(1 << (n % sizeof (*bmp))))
+
 #endif /* __UTILS_H__ */
