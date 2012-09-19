ff3a10401f1ef7b017406f46c20298f19ea62660
Oleg Belozeorov <upwawet@gmail.com>
2012-09-19 20:12:46 +0600

    monitor: implement source configuration

--- src/monitor.c
+++ src/monitor.c
@@ -3,8 +3,14 @@
 #endif /* HAVE_CONFIG_H */
 
 #include <cpssdefs.h>
+#include <gtOs/gtOsGen.h>
+#include <cpss/dxCh/dxChxGen/bridge/cpssDxChBrgVlan.h>
+#include <cpss/dxCh/dxChxGen/mirror/cpssDxChMirror.h>
 
+#include <vlan.h>
+#include <port.h>
 #include <monitor.h>
+#include <debug.h>
 
 #include <uthash.h>
 
@@ -14,6 +20,7 @@ struct session {
   int enabled;
   int nsrcs;
   struct mon_if *src;
+  struct mon_if dst;
   int rx;
   int tx;
   UT_hash_handle hh;
@@ -60,6 +67,62 @@ s_del (int num)
     return 0;
 }
 
+
+static void
+mon_configure_srcs (struct session *s)
+{
+  struct port *port;
+  int i;
+
+  for (i = 0; i < s->nsrcs; i++) {
+    switch (s->src[i].type) {
+    case MI_SRC_VLAN:
+      CRP (cpssDxChBrgVlanIngressMirrorEnable (0, s->src[i].id, GT_TRUE));
+      break;
+    case MI_SRC_PORT_RX:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorRxPortSet (port->ldev, port->lport, GT_TRUE, 0));
+      break;
+    case MI_SRC_PORT_TX:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorTxPortSet (port->ldev, port->lport, GT_TRUE, 0));
+      break;
+    case MI_SRC_PORT_BOTH:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorRxPortSet (port->ldev, port->lport, GT_TRUE, 0));
+      CRP (cpssDxChMirrorTxPortSet (port->ldev, port->lport, GT_TRUE, 0));
+    }
+  }
+}
+
+static void
+mon_deconfigure_srcs (struct session *s)
+{
+  struct port *port;
+  int i;
+
+  for (i = 0; i < s->nsrcs; i++) {
+    switch (s->src[i].type) {
+    case MI_SRC_VLAN:
+      CRP (cpssDxChBrgVlanIngressMirrorEnable (0, s->src[i].id, GT_FALSE));
+      break;
+    case MI_SRC_PORT_RX:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorRxPortSet (port->ldev, port->lport, GT_FALSE, 0));
+      break;
+    case MI_SRC_PORT_TX:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorTxPortSet (port->ldev, port->lport, GT_FALSE, 0));
+      break;
+    case MI_SRC_PORT_BOTH:
+      port = port_ptr (s->src[i].id);
+      CRP (cpssDxChMirrorRxPortSet (port->ldev, port->lport, GT_FALSE, 0));
+      CRP (cpssDxChMirrorTxPortSet (port->ldev, port->lport, GT_FALSE, 0));
+    }
+  }
+}
+
+
 enum status
 mon_session_add (mon_session_t num)
 {
@@ -74,10 +137,28 @@ mon_session_enable (mon_session_t num, int enable)
   if (!s)
     return ST_DOES_NOT_EXIST;
 
-  enable = !!enable;
-  if (s->enabled != enable) {
-    /* TODO: really enable/disable everything. */
-    s->enabled = enable;
+  if (enable && !s->enabled) {
+    if ((s->rx && en_rx) || (s->tx && en_tx))
+      return ST_BUSY;
+
+    /* TODO: configure destination. */
+
+    if (s->rx)
+      en_rx = s;
+    if (s->tx)
+      en_tx = s;
+
+    mon_configure_srcs (s);
+    s->enabled = 1;
+  } else if (s->enabled && !enable) {
+    /* TODO: deconfigure destination. */
+    if (s->rx)
+      en_rx = NULL;
+    if (s->tx)
+      en_tx = NULL;
+
+    mon_deconfigure_srcs (s);
+    s->enabled = 0;
   }
 
   return ST_OK;
@@ -93,15 +174,13 @@ enum status
 mon_session_set_src (mon_session_t num, int nsrcs, const struct mon_if *src)
 {
   struct session *s = s_get (num);
-  int rx = 0, tx = 0;
+  int rx = 0, tx = 0, i;
 
   if (!s)
     return ST_DOES_NOT_EXIST;
 
   /* Validate config. */
   if (nsrcs) {
-    int i;
-
     for (i = 0; i < nsrcs; i++) {
       switch (src[i].type) {
       case MI_SRC_VLAN:
@@ -138,7 +217,7 @@ mon_session_set_src (mon_session_t num, int nsrcs, const struct mon_if *src)
 
   if (s->src) {
     if (s->enabled) {
-      /* TODO: deconfigure sources. */
+      mon_deconfigure_srcs (s);
     }
     free (s->src);
     s->src = NULL;
@@ -146,16 +225,43 @@ mon_session_set_src (mon_session_t num, int nsrcs, const struct mon_if *src)
   }
 
   if (nsrcs) {
-    int i;
-    int size = nsrcs * sizeof (struct mon_if), i;
+    int size = nsrcs * sizeof (struct mon_if);
 
     s->src = malloc (size);
     memcpy (s->src, src, size);
     s->nsrcs = nsrcs;
-    if (enabled) {
-      /* TODO: configure sources. */
+    s->rx = rx;
+    s->tx = tx;
+    if (s->enabled) {
+      mon_configure_srcs (s);
     }
   }
+
+  return ST_OK;
+}
+
+enum status
+mon_session_set_dst (mon_session_t num, const struct mon_if *dst)
+{
+  struct session *s = s_get (num);
+
+  if (!s)
+    return ST_DOES_NOT_EXIST;
+
+  switch (dst->type) {
+  case MI_DST_VLAN:
+    if (!vlan_valid (dst->id))
+      return ST_BAD_VALUE;
+    break;
+  case MI_DST_PORT:
+    if (!port_valid (dst->id))
+      return ST_BAD_VALUE;
+    break;
+  default:
+    return ST_BAD_VALUE;
+  }
+
+  s->dst = *dst;
 
   return ST_OK;
 }
