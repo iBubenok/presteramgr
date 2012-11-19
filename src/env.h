853adc4a7d8db54bfe221e1ef9d4216ff5025aed
Oleg Belozeorov <upwawet@gmail.com>
2012-11-19 18:10:21 +0600

    reorganize hw subtypes

--- src/env.h
+++ src/env.h
@@ -1,11 +1,10 @@
 #ifndef __ENV_H__
 #define __ENV_H__
 
-#define HWST_ARLAN_3424GE     0
-#define HWST_ARLAN_3424GE_F   1
-#define HWST_ARLAN_3424GE_C   2
+#define HWST_ARLAN_3424GE_C   0
+#define HWST_ARLAN_3424GE_C_S 1
+#define HWST_ARLAN_3424GE_F   2
 #define HWST_ARLAN_3424GE_F_S 3
-#define HWST_ARLAN_3424GE_C_S 4
 
 extern void env_init (void);
 extern int env_hw_subtype (void);
