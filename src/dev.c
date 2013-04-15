8ce26d7d38d10d03998ff479c208bf24ee63a96f
Oleg Belozeorov <upwawet@gmail.com>
2013-04-15 19:43:32 +0600

    add dev_set_map()

--- src/dev.c
+++ src/dev.c
@@ -13,3 +13,11 @@ phys_dev (GT_U8 ldev)
   assert (ldev < 32);
   return dev_map[ldev];
 }
+
+void
+dev_set_map (GT_U8 ldev, GT_U8 pdev)
+{
+  assert (ldev < 32);
+  assert (pdev < 32);
+  dev_map[ldev] = pdev;
+}
--- src/dev.h
+++ src/dev.h
@@ -5,5 +5,6 @@
 #include <cpss/extServices/os/gtOs/gtGenTypes.h>
 
 extern GT_U8 phys_dev (GT_U8);
+extern void dev_set_map (GT_U8, GT_U8);
 
 #endif /* __DEV_H__ */
