282d23899de062ff411ab04624c3640e07ec9293
Oleg Belozeorov <upwawet@gmail.com>
2013-04-15 14:58:07 +0600

    add 'stack id' command line option

---
+++ src/stack.c
@@ -0,0 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <stack.h>
+
+int stack_id = 0;
---
+++ src/stack.h
@@ -0,0 +1,6 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+extern int stack_id;
+
+#endif /* __STACK_H__ */
