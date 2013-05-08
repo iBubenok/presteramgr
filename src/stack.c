794e84b2918b15e05bdd929f15afb284ed329176
Oleg Belozeorov <upwawet@gmail.com>
2013-05-08 18:00:50 +0600

    test stacking

--- src/stack.c
+++ src/stack.c
@@ -4,14 +4,55 @@
 
 #include <cpssdefs.h>
 #include <cpss/dxCh/dxChxGen/networkIf/cpssDxChNetIf.h>
+#include <cpss/generic/cscd/cpssGenCscd.h>
+#include <cpss/dxCh/dxChxGen/cscd/cpssDxChCscd.h>
 
 #include <stack.h>
 #include <vlan.h>
 #include <mcg.h>
+#include <dev.h>
+#include <port.h>
 #include <log.h>
 #include <debug.h>
 
 int stack_id = 0;
+
+enum status
+stack_set_dev_map (uint8_t tdev, uint8_t port)
+{
+  GT_STATUS rc;
+  CPSS_CSCD_LINK_TYPE_STC lp = {
+    .linkNum  = port,
+    .linkType = CPSS_CSCD_LINK_TYPE_PORT_E
+  };
+
+  rc = CRP (cpssDxChCscdDevMapTableSet (0, tdev, 0, &lp, 0));
+  switch (rc) {
+  case GT_OK:        return ST_OK;
+  case GT_HW_ERROR:  return ST_HW_ERROR;
+  case GT_BAD_PARAM: return ST_BAD_VALUE;
+  default:           return ST_HEX;
+  }
+}
+
+static void __attribute__ ((unused))
+stack_set_test_dev_map (void)
+{
+  int i, tdev;
+
+  switch (stack_id) {
+  case 1:  tdev = 2; break;
+  case 2:  tdev = 1; break;
+  default: return;
+  };
+
+  for (i = 0; i < nports; i++) {
+    if (ports[i].stack_role == PSR_PRIMARY) {
+      stack_set_dev_map (tdev, ports[i].lport);
+      break;
+    }
+  }
+}
 
 void
 stack_start (void)
@@ -19,6 +60,8 @@ stack_start (void)
   DEBUG ("doing stack setup\r\n");
   vlan_stack_setup ();
   mcg_stack_setup ();
+  /* FIXME: for testing purposes only! */
+  stack_set_test_dev_map ();
   DEBUG ("done stack setup\r\n");
 
   uint8_t tag[8];
