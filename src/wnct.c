3f2e726347ca1ee4c7dd75a6e504c7bdd7988d4c
Oleg Belozeorov <upwawet@gmail.com>
2014-02-14 15:34:42 +0600

    set up well-known control traffic on all devs

--- src/wnct.c
+++ src/wnct.c
@@ -7,6 +7,7 @@
 #include <cpss/dxCh/dxChxGen/bridge/cpssDxChBrgGen.h>
 
 #include <wnct.h>
+#include <sysdeps.h>
 #include <debug.h>
 #include <utils.h>
 
@@ -16,16 +17,19 @@ enum status
 wnct_enable_proto (uint8_t proto, int enable)
 {
   GT_STATUS rc;
+  int d;
 
   enable = !!enable;
   if (protos[proto] == enable)
     return ST_OK;
 
-  rc = CRP (cpssDxChBrgGenIeeeReservedMcastProtCmdSet
-            (0, 0, proto, enable
-             ? CPSS_PACKET_CMD_TRAP_TO_CPU_E
-             : CPSS_PACKET_CMD_FORWARD_E));
-  ON_GT_ERROR (rc) goto out;
+  for_all_devs (d) {
+    rc = CRP (cpssDxChBrgGenIeeeReservedMcastProtCmdSet
+              (d, 0, proto, enable
+               ? CPSS_PACKET_CMD_TRAP_TO_CPU_E
+               : CPSS_PACKET_CMD_FORWARD_E));
+    ON_GT_ERROR (rc) goto out;
+  }
 
   protos[proto] = enable;
 
@@ -41,11 +45,14 @@ enum status
 wnct_start (void)
 {
   GT_STATUS rc;
+  int d;
 
   memset (protos, 0, sizeof (protos));
 
-  rc = CRP (cpssDxChBrgGenIeeeReservedMcastTrapEnable (0, GT_TRUE));
-  ON_GT_ERROR (rc) goto gt_error;
+  for_all_devs (d) {
+    rc = CRP (cpssDxChBrgGenIeeeReservedMcastTrapEnable (d, GT_TRUE));
+    ON_GT_ERROR (rc) goto gt_error;
+  }
 
   return wnct_enable_proto (WNCT_STP, 1);
 
