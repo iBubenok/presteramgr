19a33a51f0e2a93c0d350d2a1d068aee735a7065
Oleg Belozeorov <upwawet@gmail.com>
2014-03-07 18:58:25 +0600

    init IP for all chips

--- src/ip.c
+++ src/ip.c
@@ -7,6 +7,7 @@
 
 #include <route.h>
 #include <ip.h>
+#include <sysdeps.h>
 #include <log.h>
 #include <debug.h>
 
@@ -19,12 +20,18 @@ ip_cpss_lib_init (void)
 enum status
 ip_start (void)
 {
-  DEBUG ("enable IPv4 link local MC reception");
-  CRP (cpssDxChBrgGenIpLinkLocalMirrorToCpuEnable
-       (0, CPSS_IP_PROTOCOL_IPV4_E, GT_TRUE));
+  int d;
 
-  CRP (cpssDxChBrgGenIpLinkLocalProtMirrorToCpuEnable
-       (0, CPSS_IP_PROTOCOL_IPV4_E, 18, GT_TRUE));
+  DEBUG ("enable IPv4 link local MC reception");
+
+  for_each_dev (d) {
+    CRP (cpssDxChBrgGenArpBcastToCpuCmdSet
+       (d, CPSS_PACKET_CMD_MIRROR_TO_CPU_E));
+    CRP (cpssDxChBrgGenIpLinkLocalMirrorToCpuEnable
+         (d, CPSS_IP_PROTOCOL_IPV4_E, GT_TRUE));
+    CRP (cpssDxChBrgGenIpLinkLocalProtMirrorToCpuEnable
+         (d, CPSS_IP_PROTOCOL_IPV4_E, 18, GT_TRUE));
+  }
 
   return ST_OK;
 }
