dc6a52a271267e1c8dd896805b90877f781f4916
Oleg Belozeorov <upwawet@gmail.com>
2014-06-03 09:27:41 +0600

    adapt multicast group support code to multidev

--- src/mcg.c
+++ src/mcg.c
@@ -10,11 +10,12 @@
 #include <debug.h>
 #include <port.h>
 #include <mcg.h>
+#include <sysdeps.h>
 
 
 struct mcast_group {
   int key;
-  CPSS_PORTS_BMP_STC ports;
+  CPSS_PORTS_BMP_STC ports[NDEVS];
   UT_hash_handle hh;
 };
 
@@ -23,10 +24,9 @@ static struct mcast_group *groups;
 enum status
 mcg_create (mcg_t mcg)
 {
-  GT_STATUS rc;
   struct mcast_group *group;
   int key = mcg;
-  int i;
+  int i, d;
 
   HASH_FIND_INT (groups, &key, group);
   if (group)
@@ -37,45 +37,32 @@ mcg_create (mcg_t mcg)
   for (i = 0; i < nports; i++) {
     struct port *port = port_ptr (i + 1);
     if (is_stack_port (port))
-      CPSS_PORTS_BMP_PORT_SET_MAC (&group->ports, port->lport);
+      CPSS_PORTS_BMP_PORT_SET_MAC (&group->ports[port->ldev], port->lport);
   }
-  rc = CRP (cpssDxChBrgMcEntryWrite (0, mcg, &group->ports));
-  if (rc == GT_OK) {
-    HASH_ADD_INT (groups, key, group);
-    return ST_OK;
+  for_each_dev (d) {
+    for (i = 0; i < dev_info[d].n_ic_ports; i++)
+      CPSS_PORTS_BMP_PORT_SET_MAC (&group->ports[d], dev_info[d].ic_ports[i]);
+    CRP (cpssDxChBrgMcEntryWrite (d, mcg, &group->ports[d]));
   }
 
-  free (group);
-  switch (rc) {
-  case GT_BAD_PARAM: return ST_BAD_VALUE;
-  case GT_HW_ERROR:  return ST_HW_ERROR;
-  default:           return ST_HEX;
-  }
+  HASH_ADD_INT (groups, key, group);
+  return ST_OK;
 }
 
 enum status
 mcg_delete (mcg_t mcg)
 {
-  GT_STATUS rc;
   struct mcast_group *group;
-  int key = mcg;
+  int key = mcg, d;
 
   HASH_FIND_INT (groups, &key, group);
   if (!group)
     return ST_DOES_NOT_EXIST;
 
-  rc = CRP (cpssDxChBrgMcGroupDelete (0, mcg));
-  if (rc == GT_OK) {
-    HASH_DEL (groups, group);
-    free (group);
-    return ST_OK;
-  }
+  for_each_dev (d)
+    CRP (cpssDxChBrgMcGroupDelete (d, mcg));
 
-  switch (rc) {
-  case GT_BAD_PARAM: return ST_DOES_NOT_EXIST;
-  case GT_HW_ERROR:  return ST_HW_ERROR;
-  default:           return ST_HEX;
-  }
+  return ST_OK;
 }
 
 enum status
@@ -94,12 +81,12 @@ mcg_add_port (mcg_t mcg, port_id_t pid)
   if (!group)
     return ST_DOES_NOT_EXIST;
 
-  if (CPSS_PORTS_BMP_IS_PORT_SET_MAC (&group->ports, port->lport))
+  if (CPSS_PORTS_BMP_IS_PORT_SET_MAC (&group->ports[port->ldev], port->lport))
     return ST_ALREADY_EXISTS;
 
-  rc = CRP (cpssDxChBrgMcMemberAdd (0, mcg, port->lport));
+  rc = CRP (cpssDxChBrgMcMemberAdd (port->ldev, mcg, port->lport));
   if (rc == GT_OK) {
-    CPSS_PORTS_BMP_PORT_SET_MAC (&group->ports, port->lport);
+    CPSS_PORTS_BMP_PORT_SET_MAC (&group->ports[port->ldev], port->lport);
     return ST_OK;
   }
 
@@ -125,12 +112,12 @@ mcg_del_port (mcg_t mcg, port_id_t pid)
   if (!group)
     return ST_DOES_NOT_EXIST;
 
-  if (!CPSS_PORTS_BMP_IS_PORT_SET_MAC (&group->ports, port->lport))
+  if (!CPSS_PORTS_BMP_IS_PORT_SET_MAC (&group->ports[port->ldev], port->lport))
     return ST_DOES_NOT_EXIST;
 
-  rc = CRP (cpssDxChBrgMcMemberDelete (0, mcg, port->lport));
+  rc = CRP (cpssDxChBrgMcMemberDelete (port->ldev, mcg, port->lport));
   if (rc == GT_OK) {
-    CPSS_PORTS_BMP_PORT_CLEAR_MAC (&group->ports, port->lport);
+    CPSS_PORTS_BMP_PORT_CLEAR_MAC (&group->ports[port->ldev], port->lport);
     return ST_OK;
   }
 
@@ -143,10 +130,15 @@ mcg_del_port (mcg_t mcg, port_id_t pid)
 void
 mcg_dgasp_setup (void)
 {
-  CPSS_PORTS_BMP_STC bmp;
+  CPSS_PORTS_BMP_STC bmp[NDEVS];
+  int d, i;
 
-  memset (&bmp, 0, sizeof (bmp));
-  CRP (cpssDxChBrgMcEntryWrite (0, DGASP_MCG, &bmp));
+  memset (bmp, 0, sizeof (bmp));
+  for_each_dev (d) {
+    for (i = 0; i < dev_info[d].n_ic_ports; i++)
+      CPSS_PORTS_BMP_PORT_SET_MAC (&bmp[d], dev_info[d].ic_ports[i]);
+    CRP (cpssDxChBrgMcEntryWrite (d, DGASP_MCG, &bmp[d]));
+  }
 }
 
 enum status
@@ -160,9 +152,9 @@ mcg_dgasp_port_op (port_id_t pid, int add)
     return ST_BAD_VALUE;
 
   if (add)
-    rc = CRP (cpssDxChBrgMcMemberAdd (0, DGASP_MCG, port->lport));
+    rc = CRP (cpssDxChBrgMcMemberAdd (port->ldev, DGASP_MCG, port->lport));
   else
-    rc = CRP (cpssDxChBrgMcMemberDelete (0, DGASP_MCG, port->lport));
+    rc = CRP (cpssDxChBrgMcMemberDelete (port->ldev, DGASP_MCG, port->lport));
 
   switch (rc) {
   case GT_OK:       return ST_OK;
