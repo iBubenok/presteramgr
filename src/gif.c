770c7fe3b791fc1ce57faecf81f1afe5679a329b
Mikhail Khotko <mkhotko@debian>
2015-08-28 19:50:30 +0500

    gif_tx: put vlan tag if 1) GIFD_PORT, 2) port is trunk, 3) native vlan != desired vlan

--- src/gif.c
+++ src/gif.c
@@ -154,8 +154,8 @@ gif_tx (const struct gif_id *id,
   switch (opts->send_to)
   {
     case GIFD_PORT: {
-      DEBUG("%s: GIFD_PORT: vlan: %d hw_port: %d\r\n", __FUNCTION__,
-        opts->vid, hp.hw_port);
+      DEBUG("%s: GIFD_PORT: vlan: %d port_num: %d hw_port: %d\r\n",
+        __FUNCTION__, opts->vid, id->num, hp.hw_port);
       tp.commonParams.dsaTagType = CPSS_DXCH_NET_DSA_TYPE_EXTENDED_E;
       tp.commonParams.vid = opts->vid;
       tp.commonParams.vpt = 7;
@@ -167,6 +167,15 @@ gif_tx (const struct gif_id *id,
       tp.dsaInfo.fromCpu.cascadeControl = gt_bool (opts->ignore_stp);
       tp.dsaInfo.fromCpu.srcDev = stack_id;
       tp.dsaInfo.fromCpu.srcId = stack_id;
+
+      struct port *port = port_ptr (id->num);
+      DEBUG("%s: port->mode == PM_TRUNK: %d port->native_vid: %d\r\n",
+        __FUNCTION__, (port->mode == PM_TRUNK), port->native_vid);
+      if ((port->mode == PM_TRUNK) && (port->native_vid != opts->vid)) {
+        DEBUG("%s: dstIsTagged = GT_TRUE\r\n", __FUNCTION__);
+        tp.dsaInfo.fromCpu.extDestInfo.devPort.dstIsTagged = GT_TRUE;
+      }
+
     } break;
 
     case GIFD_VLAN: {
@@ -192,6 +201,8 @@ gif_tx (const struct gif_id *id,
     } break;
 
     case GIFD_VIDX: {
+      DEBUG("%s: GIFD_VIDX: vlan: %d\r\n", __FUNCTION__,
+        opts->vid);
       tp.commonParams.dsaTagType = CPSS_DXCH_NET_DSA_TYPE_EXTENDED_E;
       tp.commonParams.vid = opts->vid;
       tp.commonParams.vpt = 7;
