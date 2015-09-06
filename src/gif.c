c11bc66e85b63c6a0296a3fe29fb11cb234bc449
Ildar Kindibulatov <ikindibulatov@plgn.ru>
2015-09-06 22:38:11 +0500

    quickfix for broken LLDP transmission. to be reverted

--- src/gif.c
+++ src/gif.c
@@ -164,7 +164,7 @@ gif_tx (const struct gif_id *id,
       tp.dsaInfo.fromCpu.dstInterface.devPort.devNum = hp.hw_dev;
       tp.dsaInfo.fromCpu.dstInterface.devPort.portNum = hp.hw_port;
       tp.dsaInfo.fromCpu.cascadeControl = gt_bool (opts->ignore_stp);
-      tp.dsaInfo.fromCpu.egrFilterEn = gt_bool ( !opts->ignore_stp);
+//      tp.dsaInfo.fromCpu.egrFilterEn = gt_bool ( !opts->ignore_stp);
       tp.dsaInfo.fromCpu.srcDev = stack_id;
       tp.dsaInfo.fromCpu.srcId = stack_id;
 
