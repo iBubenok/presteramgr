1e6c59a048821e6e533ab98ddffe4d661627dbb0
Sergey Efremov <sefremov@plgn.ru>
2015-10-12 14:27:44 +0500

    MVR: add IPv4 IGMP package hack for vlan id

--- src/control.c
+++ src/control.c
@@ -1961,7 +1961,20 @@ DEFINE_HANDLER (CC_INT_SPEC_FRAME_FORWARD)
     put_vlan_id (msg, frame->vid);
   put_port_id (msg, pid);
 
-  zmsg_addmem (msg, frame->data, frame->len);
+  // Mud mad hack  - added dirty duty hack? kif
+
+  unsigned char buf[60];
+
+  if ((type == CN_IPv4_IGMP_PDU) && (frame->len == 56)) {
+    memset (buf, 0, 60);
+    memcpy (buf, frame->data, frame->len);
+    zmsg_addmem (msg, buf, 60);
+  } else
+    zmsg_addmem (msg, frame->data, frame->len);
+
+  // End of Mud mad hack
+
+  //zmsg_addmem (msg, frame->data, frame->len);
 
   switch (type) {
     case CN_ARP_BROADCAST:
