2ea7353e23a63847117a9b76c3efc641fa17829c
Mikhail Khotko <mkhotko@debian>
2016-10-24 13:25:33 +0500

    Do NOT flush per vlan in MSTP mode

--- src/control.c
+++ src/control.c
@@ -1616,23 +1616,26 @@ DEFINE_HANDLER (CC_VIF_FDB_FLUSH)
     goto out;
 
   arg.vifid = vif;
+  arg.vid = ALL_VLANS;
+  result = mac_flush_vif (&arg, GT_FALSE);
 
-  if (POP_ARG(&stp_id) == ST_OK) {
-    vid_t vid;
-    for (vid = 1; vid <= NVLANS; vid++) {
-      stp_id_t vlan_stp_id;
-      if ((vlan_get_stp_id(vid, &vlan_stp_id) == ST_OK) &&
-          (stp_id == vlan_stp_id)) {
-        arg.vid = vid;
-        result = mac_flush_vif (&arg, GT_FALSE);
-      }
-      if (result != ST_OK)
-        goto out;
-    }
-  } else {
-    arg.vid = ALL_VLANS;
-    result = mac_flush_vif (&arg, GT_FALSE);
-  }
+  // @todo:
+  // if (POP_ARG(&stp_id) == ST_OK) {
+  //   vid_t vid;
+  //   for (vid = 1; vid <= NVLANS; vid++) {
+  //     stp_id_t vlan_stp_id;
+  //     if ((vlan_get_stp_id(vid, &vlan_stp_id) == ST_OK) &&
+  //         (stp_id == vlan_stp_id)) {
+  //       arg.vid = vid;
+  //       result = mac_flush_vif (&arg, GT_FALSE);
+  //     }
+  //     if (result != ST_OK)
+  //       goto out;
+  //   }
+  // } else {
+  //   arg.vid = ALL_VLANS;
+  //   result = mac_flush_vif (&arg, GT_FALSE);
+  // }
 
  out:
   report_status (result);
