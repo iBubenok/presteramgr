b43ada6b609ec2fda9d22fb4a6af3bfba03a1f50
Mikhail Khotko <mkhotko@plgn.ru>
2017-10-23 13:51:18 +0500

    Add dump of XG port QT2025 PHY

--- src/diag.h
+++ src/diag.h
@@ -13,5 +13,8 @@ extern enum status diag_bic_set_mode (uint8_t, uint8_t, uint8_t, vid_t);
 extern enum status diag_bic_read (uint8_t, uint32_t *);
 extern enum status diag_desc_read (uint8_t, uint8_t *, uint32_t *);
 extern enum status diag_read_ret_cnt (uint8_t, uint32_t *);
+extern enum status diag_dump_xg_port_qt2025_start (port_id_t, const char *);
+extern enum status diag_dump_xg_port_qt2025_check (bool_t *);
+extern enum status diag_dump_xg_port_qt2025 (port_id_t, uint32_t, uint32_t, uint16_t*);
 
 #endif /* __DIAG_H__ */
