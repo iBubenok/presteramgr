a09ef2a27a73cf91abde9df03c88cbf498bf44e0
Ekaterina Safiullina <esafiullina@plgn.ru>
2018-02-19 12:04:13 +0500

    remove unused commands

--- src/control.c
+++ src/control.c
@@ -506,15 +506,6 @@ DECLARE_HANDLER (CC_PORT_GET_SERDES_CFG);
 DECLARE_HANDLER (CC_PORT_SET_SERDES_CFG);
 DECLARE_HANDLER (CC_GET_PORT_IP_SOURCEGUARD_RULE_START_IX);
 DECLARE_HANDLER (CC_GET_PER_PORT_IP_SOURCEGUARD_RULES_COUNT);
-DECLARE_HANDLER (CC_SOURCE_GUARD_ENABLE_TRAP);
-DECLARE_HANDLER (CC_SOURCE_GUARD_DISABLE_TRAP);
-DECLARE_HANDLER (CC_SOURCE_GUARD_ENABLE_DROP);
-DECLARE_HANDLER (CC_SOURCE_GUARD_DISABLE_DROP);
-DECLARE_HANDLER (CC_SOURCE_GUARD_VIF_ENABLE);
-DECLARE_HANDLER (CC_SOURCE_GUARD_SET_VIF_VERIFY_MAC);
-DECLARE_HANDLER (CC_SOURCE_GUARD_ADD);
-DECLARE_HANDLER (CC_SOURCE_GUARD_DELETE);
-DECLARE_HANDLER (CC_SOURCE_GUARD_SHOW_TABS);
 DECLARE_HANDLER (CC_USER_ACL_SET);
 DECLARE_HANDLER (CC_USER_ACL_RESET);
 DECLARE_HANDLER (CC_USER_ACL_FAKE_MODE);
@@ -699,15 +690,6 @@ static cmd_handler_t handlers[] = {
   HANDLER (CC_PORT_SET_SERDES_CFG),
   HANDLER (CC_GET_PORT_IP_SOURCEGUARD_RULE_START_IX),
   HANDLER (CC_GET_PER_PORT_IP_SOURCEGUARD_RULES_COUNT),
-  HANDLER (CC_SOURCE_GUARD_ENABLE_TRAP),
-  HANDLER (CC_SOURCE_GUARD_DISABLE_TRAP),
-  HANDLER (CC_SOURCE_GUARD_ENABLE_DROP),
-  HANDLER (CC_SOURCE_GUARD_DISABLE_DROP),
-  HANDLER (CC_SOURCE_GUARD_VIF_ENABLE),
-  HANDLER (CC_SOURCE_GUARD_SET_VIF_VERIFY_MAC),
-  HANDLER (CC_SOURCE_GUARD_ADD),
-  HANDLER (CC_SOURCE_GUARD_DELETE),
-  HANDLER (CC_SOURCE_GUARD_SHOW_TABS),
   HANDLER (CC_USER_ACL_SET),
   HANDLER (CC_USER_ACL_RESET),
   HANDLER (CC_USER_ACL_FAKE_MODE),
@@ -5030,161 +5012,6 @@ DEFINE_HANDLER (CC_GET_PER_PORT_IP_SOURCEGUARD_RULES_COUNT)
   zmsg_t *reply = make_reply (ST_OK);
   zmsg_addmem (reply, &count, sizeof (count));
   send_reply (reply);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_ENABLE_TRAP)
-{
-  enum status result;
-  port_id_t pid;
-
-  if ((result = POP_ARG (&pid)) != ST_OK)
-    goto out;
-
-  pcl_source_guard_trap_enable (pid);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_DISABLE_TRAP)
-{
-  enum status result;
-  port_id_t pid;
-
-  if ((result = POP_ARG (&pid)) != ST_OK)
-    goto out;
-
-  pcl_source_guard_trap_disable (pid);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_ENABLE_DROP)
-{
-  enum status result;
-  port_id_t pid;
-
-  if ((result = POP_ARG (&pid)) != ST_OK)
-    goto out;
-
-  pcl_source_guard_drop_enable (pid);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_DISABLE_DROP)
-{
-  enum status result;
-  port_id_t pid;
-
-  if ((result = POP_ARG (&pid)) != ST_OK)
-    goto out;
-
-  pcl_source_guard_drop_disable (pid);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_VIF_ENABLE)
-{
-  enum status result;
-  vif_id_t vifid;
-  bool_t enable;
-
-  if ((result = POP_ARG (&vifid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&enable)) != ST_OK)
-    goto out;
-
-  // enable_vif (vifid, enable);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_SET_VIF_VERIFY_MAC)
-{
-  enum status result;
-  vif_id_t vifid;
-  bool_t verify_mac;
-
-  if ((result = POP_ARG (&vifid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&verify_mac)) != ST_OK)
-    goto out;
-
-  // set_vif_verify_mac (vifid, verify_mac);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_ADD)
-{
-  enum status result;
-  vif_id_t vifid;
-  mac_addr_t mac;
-  vid_t vid;
-  ip_addr_t ip;
-  uint8_t type;
-  // bool verify_mac;
-
-  if ((result = POP_ARG (&vifid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&mac)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&vid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&ip)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&type)) != ST_OK)
-    goto out;
-  // if ((result = POP_ARG (&verify_mac)) != ST_OK)
-  //   goto out;
-
-  // add_bind (vifid, mac, vid, ip, type);
-
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_DELETE)
-{
-  DEBUG ("cc source guard delete");
-  enum status result;
-  vif_id_t vifid;
-  mac_addr_t mac;
-  vid_t vid;
-  ip_addr_t ip;
-  uint8_t type;
-
-  if ((result = POP_ARG (&vifid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&mac)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&vid)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&ip)) != ST_OK)
-    goto out;
-  if ((result = POP_ARG (&type)) != ST_OK)
-    goto out;
-
-  // delete_bind (vifid, mac, vid, ip, type);
-  result = ST_OK;
- out:
-  report_status (result);
-}
-
-DEFINE_HANDLER (CC_SOURCE_GUARD_SHOW_TABS)
-{
-  // show_binds_tab ();
-  DEBUG ("\n");
-
-  report_status (ST_OK);
 }
 
 #define INIT_VAR(var) do {                          \
