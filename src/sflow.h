f27bd4a8dfae80a5b3e2923af821969222c7d7c2
Vladimir Samorodov <vsamorodov@akber-soft.ru>
2021-06-16 18:25:58 +0500

    Add converter for GT_STATUS

--- src/sflow.h
+++ src/sflow.h
@@ -2,25 +2,30 @@
 #include <control-proto.h>
 
 /*Global enable*/
-GT_STATUS
+enum status
 sflow_set_enable (
   sflow_type_t type,
   int enable);
 
 /**/
-GT_STATUS
+enum status
 sflow_set_ingress_count_mode (
   sflow_count_mode_t mode);
 
 /**/
-GT_STATUS
+enum status
 sflow_set_reload_mode (
   sflow_type_t type,
   sflow_count_reload_mode_t mode);
 
 /*Port limit*/
-GT_STATUS
+enum status
 sflow_set_port_limit (
   port_id_t pid,
   sflow_type_t type,
   uint32_t limit);
+
+/*Convert GT_STATUS to enum status*/
+enum status
+convert_status (
+    GT_STATUS st);
